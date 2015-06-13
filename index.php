a16e5152cec5d8f4f6813eb7aae5d714c0a4fd04
unknown <a516067@lb020741.dmn1.fmr.com>
2015-06-13 16:18:58 -0500

    Added some graphics enhancements in the website

--- index.php
+++ index.php
@@ -9,20 +9,28 @@ session_start();
 		<title>URL Shortener</title>
 		<link rel="stylesheet" href="css/global.css">
 	</head>
-	<body>
+	<body background="bg2.jpg">
 		<div class="container"> 
-			<h1 class="title">Shorten a URL.</h1>
+			<h1 class="title">Shorten a URL</h1>
 
 			<?php
+
 			if(isset($_SESSION['feedback'])){
+			?>	
+			<script type="text/javascript">
+				document.body.style.backgroundImage="url('bg3.jpg')";
+			</script>
+			<?php
 				echo "<p>{$_SESSION['feedback']}</p>";
-				unset($_SESSION['feedback']);
-			}
+				unset($_SESSION['feedback']);																				
+			} else{
+				echo "<p>Brighten the day...</p>";
+			}			
 			?>
 
 			<form action="shorten.php" method="post">
-				<input type="url" name="url" placeholder="Enter a URL here." autocomplete="off">
-				<input type="submit" value="Shorten">
+				<input type="url" name="url" id ="url" value="http://" autocomplete="off">							
+				<input type="submit" name="submit" value="Shorten">
 			</form>	
 		</div>
 	</body>
--- shorten.php
+++ shorten.php
@@ -8,7 +8,7 @@ if(isset($_POST['url'])){
 	$url = $_POST['url'];
 
 	if ($code = $s->makeCode($url)){
-		$_SESSION['feedback'] = "Generated! Your short URL is: <a href=\"/{$code}\">dira89.com/{$code}</a>";
+		$_SESSION['feedback'] = "Much Better.... Short URL : <a href=\"/{$code}\">dira89.com/{$code}</a>";
 	} else{
 		$_SESSION['feedback'] = "There was a problem. Invalid URL";
 	}
